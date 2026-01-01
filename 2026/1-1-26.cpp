6dce57e7f27484b866aadecb030827254cd157b5
MRKARTIK001 <bajpaikartik001@gmail.com>
2026-01-01 12:56:54 +0530

    HAPPY NEW YEAR

---
+++ 2026/1-1-26.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> plusOne(vector<int>& digits) {
+    int n = digits.size();
+    int carry = 1;
+    for (int i = n - 1; i >= 0; i--) {
+        int sum = digits[i] + carry;
+        digits[i] = sum % 10;
+        carry = sum / 10;
+    }
+    if (carry > 0) {
+        digits.insert(digits.begin(), carry);
+    }
+    return digits;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    vector<int> result = plusOne(arr);
+    for (int num : result) {
+        cout << num << " ";
+    }
+    cout << endl;
+    return 0;
+}
